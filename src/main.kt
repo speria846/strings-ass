dbfa885699a5436b3efa4b7c0b12c2c94675a923
speria846 <niwenyesigasperia@gmail.com>
2022-03-01 22:58:47 +0300

    third commit

---
+++ src/main.kt
@@ -0,0 +1,23 @@
+fun main(){
+ var text = "akirachix"
+    println(text[0].toString()+text[2]+text[3])
+    var n =structure("speria",22)
+    println(n)
+    var m=doc()
+    println(m)
+    me("niwenyesiga")
+    }
+fun structure(x:String,y:Int):String{
+    var structure ="hi,my name is $x  and am $y years old"
+    return structure
+}
+fun doc(): Int {
+var doc ="instrument"
+ var lenghs = doc.length
+ return lenghs
+}
+fun me(name:String){
+    if(name == "speria")
+println("that is not me")
+    println("i dont know who that is")
+}
